05cdb06844a55c3c9e6978a5825ab15efa6a4b1f
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-02-11 19:26:20 +0100

    Etape 5 : Finalisez la commande list

--- ContactManager.php
+++ ContactManager.php
@@ -22,14 +22,11 @@ class ContactManager {
         $contact->setId($data['id'])
                 ->setName($data['name'])
                 ->setEmail($data['email'])
-                // CORRECTION 1 : On utilise la variable, pas une string
                 ->setPhoneNumber($data['phone_number']); 
         
-        // CORRECTION 2 : On ajoute au tableau avec []
         $contacts[] = $contact; 
     }
 
-    // CORRECTION 3 : On retourne le tableau final
     return $contacts; 
   }
 
